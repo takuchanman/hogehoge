8f3f557fe16b76e3337c6517b4c5047330a598af
Araki Taku <arakitaku@Tak-Mac-Pro-01.AirPort>
2020-03-15 19:48:35 +0900

    necochan 変更

--- necochan.rb
+++ necochan.rb
@@ -14,23 +14,23 @@
 
 
 
-# puts "今年は何年？？"
-# x = gets.to_i
-# if x % 400 == 0
-#   puts "#{x}年はうるう年です"
-# elsif x % 4 == 0 && x % 100 != 0
-#   puts "#{x}年はうるう年です"
-# else
-#   puts "#{x}年はうるう年ではありません"
-# end
-
-
-(1..40).each do |i|
-  if i % 3 == 0
-    puts "Afo"
-  elsif i.to_s.include?("3")
-    puts "Afo"
-  else
-    puts i
-  end
+puts "今年は何年？？"
+x = gets.to_i
+if x % 400 == 0
+  puts "#{x}年はうるう年です"
+elsif x % 4 == 0 && x % 100 != 0
+  puts "#{x}年はうるう年です"
+else
+  puts "#{x}年はうるう年ではありません"
 end
+
+
+# (1..40).each do |i|
+#   if i % 3 == 0
+#     puts "Afo"
+#   elsif i.to_s.include?("3")
+#     puts "Afo"
+#   else
+#     puts i
+#   end
+# end
